c38adbc7e0440a49d3cd0518aa9b6f2af203f580
HarleyC0 <gerson_harley@hotmail.com>
2024-12-16 18:56:50 -0500

    obtener datos del form

--- app.js
+++ app.js
@@ -3,8 +3,30 @@ const path = require('path')
 const app = express()
 const port = 3000;
 
+
+// Middleware para servir archivos estáticos
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
+
+// Leer datos de form
+
+app.use(express.urlencoded({ extended: true }));
+
+app.post('/submit', (req, res) => {
+    const {
+        'srA': srA,
+        'mrS': mrS,
+        'name': name,
+        'fechaEs': fechaEs,
+        'fechaEn': fechaEn,
+        'dir1': dir1,
+        'dir2': dir2,
+        'num': num,
+        'email': email,
+        'nameAgent': nameAgent,
+        } = req.body;
+    console.log(`nombre: ${name}, correo: ${email}`)
+    res.send(`Datos recibidos: <br> Nombre: ${name} <br> Correo: ${email}`);
+});
+
+app.listen(port)
