a84ef75a9f909970c1f9ea17d4a69607d27622d2
HarleyC0 <gerson_harley@hotmail.com>
2025-02-04 18:33:34 -0500

    descarga directa sin necesidad de usar carpetas locales

--- Bienvenida.js
+++ Bienvenida.js
@@ -1,57 +1,59 @@
-const { PDFDocument, StandardFonts, rgb } = require('pdf-lib')
-const fontkit = require('@pdf-lib/fontkit')
-const fs = require('fs')
-// direccion a fuente
-//const fontBytes = fs.readFileSync('./fonts/ARIALBOLDMT.OTF')
+const { PDFDocument, StandardFonts, rgb } = require('pdf-lib');
+const fontkit = require('@pdf-lib/fontkit');
+const fs = require('fs');
 const path = require('path');
+
+// Cargar la fuente desde el directorio público
 const fontPath = path.join(process.cwd(), 'public', 'fonts', 'ARIALBOLDMT.OTF');
 const fontBytes = fs.readFileSync(fontPath);
 
-
-
 async function modifyPdf(sr, nameClient, fechaEs) {
-    //const pdfDoc = await PDFDocument.load(fs.readFileSync('./public/pdfs/Bienvenida.pdf'));
-    const pdfDoc = await PDFDocument.load(fs.readFileSync(path.join(process.cwd(), 'public', 'pdfs', 'Bienvenida.pdf')));
+    try {
+        // Cargar el documento PDF base
+        const pdfDoc = await PDFDocument.load(fs.readFileSync(path.join(process.cwd(), 'public', 'pdfs', 'Bienvenida.pdf')));
 
-    pdfDoc.registerFontkit(fontkit);
-    const ArialFuente = await pdfDoc.embedFont(fontBytes);
+        // Registrar la fuente
+        pdfDoc.registerFontkit(fontkit);
+        const ArialFuente = await pdfDoc.embedFont(fontBytes);
 
-    const pages = pdfDoc.getPages();
-    const firstPage = pages[0];
-  
-    firstPage.drawText(sr, {
-        x: 72,
-        y: 552,
-        size: 12,
-        font: ArialFuente,
-        color: rgb(0, 0, 0),
-    });    
+        const pages = pdfDoc.getPages();
+        const firstPage = pages[0];
 
-    firstPage.drawText(nameClient, {
-        x: 72,
-        y: 540,
-        size: 12,
-        font: ArialFuente,
-        color: rgb(0, 0, 0),
-    });
+        // Dibujar el texto en el PDF
+        firstPage.drawText(sr, {
+            x: 72,
+            y: 552,
+            size: 12,
+            font: ArialFuente,
+            color: rgb(0, 0, 0),
+        });
 
-    firstPage.drawText(fechaEs, {
-        x: 72,
-        y: 620,
-        size: 12,
-        font: ArialFuente,
-        color: rgb(0, 0, 0),
-    });   
+        firstPage.drawText(nameClient, {
+            x: 72,
+            y: 540,
+            size: 12,
+            font: ArialFuente,
+            color: rgb(0, 0, 0),
+        });
 
-    const pdfBytes = await pdfDoc.save();
-    const cleanName = nameClient.replace(/\s+/g, '');
+        firstPage.drawText(fechaEs, {
+            x: 72,
+            y: 620,
+            size: 12,
+            font: ArialFuente,
+            color: rgb(0, 0, 0),
+        });
 
-    console.log("pdf creado pero no guardado")
-    // save en carpeta temporal de vercel
-    const tempFilePath = `tmp/Bienvenida${cleanName}.pdf`;
-    fs.writeFileSync(tempFilePath, pdfBytes);
-
-    console.log("PDF modificado y guardado en 'output/1.Bienvenida.pdf")
+        // Guardar el PDF en memoria como un Buffer
+        const pdfBytes = await pdfDoc.save();
+        const tempFilePath = `tmp/BienvenidaPruebaBuffer.pdf`;
+        fs.writeFileSync(tempFilePath, pdfBytes);
+        // Devolver el PDF como un Buffer
+        return pdfBytes; // El Buffer se puede enviar directamente en la respuesta HTTP
+    } catch (error) {
+        console.error('Error generando el PDF:', error);
+        throw error;
+    }
 }
 
 module.exports = modifyPdf;
--- index.js
+++ index.js
@@ -1,30 +1,26 @@
-const express = require('express')
-const path = require('path')
-const index = express()
+const express = require('express');
+const path = require('path');
+const index = express();
 
-const port = process.env.PORT || 3000; // de forma local 3000, en vercel asignara un puerto
+const port = process.env.PORT || 3000; // De forma local 3000, en Vercel asignará un puerto
 
-const modifyPdf = require('./Bienvenida')
-const AcuerdoServicios = require('./AcuerdoServicios')
+const modifyPdf = require('./Bienvenida');
+const AcuerdoServicios = require('./AcuerdoServicios');
 
-// ruta para servir el index.html en pagina principal
+// Ruta para servir el index.html en la página principal
 index.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 // Middleware para servir archivos estáticos (permite encontrar y servir styles.css)
-index.use(express.static(path.join(process.cwd(), 'public')))
+index.use(express.static(path.join(process.cwd(), 'public')));
 
 index.get('/status', (req, res) => {
-    res.send({
-        up: true
-    });
+    res.send({ up: true });
 });
 
 // Middleware para leer datos del formulario
-
 index.use(express.urlencoded({ extended: true }));
-
 
 index.post('/submit', async (req, res) => {
     const {
@@ -38,42 +34,28 @@ index.post('/submit', async (req, res) => {
         'num': num,
         'email': email,
         'nameAgent': nameAgent,
-        } = req.body;
-    console.log(`Datos recibidos: Nombre ${name}, correo: ${email}`)
-    
+    } = req.body;
+
+    console.log(`Datos recibidos: Nombre ${name}, correo: ${email}`);
+
     try {
-        await modifyPdf(`${srA || mrS}`, name, fechaEs)
-        await AcuerdoServicios(fechaEs, fechaEn, name)
-        // Meter aqui el link de "Descargar PDF"
-        const cleanName = name.replace(/\s+/g, '');
-        res.send(`
-            <h2>PDF generado para: ${cleanName}.</h2>
-            <p><a href=/download?file=Bienvenida${cleanName}.pdf>Descargar Bienvenida ${name}</a></p>
-            <p><a href=/download?file=CertificacionDeVerdad${cleanName}.pdf>Descargar CertificacionDeVerdad ${name}</a></p>
-            <p><a href=/download?file=DeclaracionyCertificacion${cleanName}.pdf>Descargar DeclaracionyCertificacion ${name}</a></p>
-            <p><a href=/download?file=AcuerdosDeServicio${cleanName}.pdf>Descargar AcuerdosDeServicio ${name}</a></p>
-            <p><a href=/download?file=EsquemaPago${cleanName}.pdf>Descargar EsquemaPago ${name}</a></p>
-            <p><a href=/download?file=Pagare${cleanName}.pdf>Descargar Pagare ${name}</a></p>
-            <p><a href=/download?file=RenunciaResponsabilidad${cleanName}.pdf>Descargar RenunciaResponsabilidad ${name}</a></p>
-            <p><a href=/download?file=MetodosDePago${cleanName}.pdf>Descargar MetodosDePago ${name}</a></p>
-        `)
+        // Generar los PDFs en memoria
+        const bienvenidaPdf = await modifyPdf(`${srA || mrS}`, name, fechaEs);
+        const acuerdoPdf = await AcuerdoServicios(fechaEs, fechaEn, name);
+
+        // Enviar los PDFs directamente como respuesta
+        res.setHeader('Content-Type', 'application/pdf');
+        res.setHeader('Content-Disposition', `inline; filename="Bienvenida.pdf"`);
+
+        // Si quieres enviar el PDF de Bienvenida
+        res.send(bienvenidaPdf);
+
+        // Si deseas enviar el Acuerdo de Servicios como PDF en lugar de respuesta directa, puedes hacerlo aquí también:
+        // res.send(acuerdoPdf); 
     } catch (error) {
-        console.log("Error generando el PDF", error)
-        res.status(500).send("Ocurrio un error generando el pdf");
+        console.log("Error generando el PDF", error);
+        res.status(500).send("Ocurrió un error generando el PDF");
     }
-});
-
-// Nueva ruta para manejar la descarga de archivos
-index.get('/download', (req, res) => {
-    const fileName = req.query.file; // Obtener el nombre del archivo desde la URL
-    const filePath = path.join(__dirname, 'tmp', fileName);
-
-    res.download(filePath, fileName, (err) => {
-        if (err) {
-            console.error('Error al descargar el archivo:', err);
-            res.status(500).send('Error al descargar el archivo.');
-        }
-    });
 });
 
 index.listen(port, () => {
